5ad31da3907d3e99381c46ed5159ec2df56dea6a
Mirelle Wallgren <mirelle.wallgren@medieinstitutet.se>
2023-04-18 13:32:48 +0200

    added file structure

--- therestaurant/src/App.js
+++ therestaurant/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Booking from './components/Booking';
+import Contact from './components/Contact';
+import { LandingPage } from './components/landingPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <LandingPage />
+      <Booking />
+      <Contact />
+    </div> 
       
-    </div>
   );
 }
 
