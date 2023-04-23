8f3f4cfda5a0793ba1dd6a3acc9883816bf37747
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-04-23 16:14:57 +0200

    Hämtat funktioner från smarta kontraktet. bookings och create restaurant borde vara ok!

--- therestaurant/src/components/Booking/BookingFormContainer.jsx
+++ therestaurant/src/components/Booking/BookingFormContainer.jsx
@@ -1,19 +1,17 @@
-import React from "react";
-import BookingForm from "./BookingForm";
-import { createBooking } from "../../Services/web3Service";
+// import React from "react";
+// import { BookingForm } from "./BookingForm";
+// import { createBooking } from "../../Services/web3Service";
 
-const BookingFormContainer = () => {
-  const handleSubmit = async (bookingData) => {
-    createBooking(bookingData, (error, message) => {
-      if (error) {
-        console.error("Error:", error);
-      } else {
-        console.log("Message:", message);
-      }
-    });
-  };
+// export const BookingFormContainer = () => {
+//   const handleSubmit = async (bookingData) => {
+//     createBooking(bookingData, (error, message) => {
+//       if (error) {
+//         console.error("Error:", error);
+//       } else {
+//         console.log("Message:", message);
+//       }
+//     });
+//   };
 
-  return <BookingForm onSubmit={handleSubmit} />;
-};
-
-export default BookingFormContainer;
+//   return <BookingForm onSubmit={handleSubmit} />;
+// };
