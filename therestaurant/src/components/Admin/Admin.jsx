001536187ffc640c15aeac58b300a8445849902b
A. Mirelle Wallgren <mirelle.wallgren@medieinstitutet.se>
2023-04-24 11:25:34 +0200

    test

--- therestaurant/src/components/Admin/Admin.jsx
+++ therestaurant/src/components/Admin/Admin.jsx
@@ -3,7 +3,7 @@
  export const Admin = () => {
     return (
         <div>
-            <h1>This is the admin page, 
+            <h1>This is the admin page,
                 which is currently under construction.
                 Please stand by and wait for something amazing to happen... SOON. 
             </h1>
